3e81d1c7ae2d35d917a5161ceba4441c0b14da09
Joyce Cardoso de Araújo <joycecardoso@discente.ufg.br>
2021-03-10 22:08:34 -0300

    Ex4 - Criar a classe NomeSobrenome-joyce

---
+++ JoyceCardoso.java
@@ -0,0 +1,15 @@
+public class JoyceCardoso
+{
+    String nome, sobrenome;
+    int semestre;
+  
+    public String nomeCompleto(){
+        String nomeCompleto = nome + " " + sobrenome;
+        return nomeCompleto;
+    }
+    
+    public int getSemestre(){
+        int getSemestre = semestre; 
+        return getSemestre;
+    }
+}
