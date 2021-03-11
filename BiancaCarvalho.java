6cf7c5b3b3a04de0378ed7cc09db6f2c986e6d56
Bianca Carvalho <biancapcarvalho@outlook.com>
2021-03-10 22:02:58 -0300

    classe BiancaCarvalho

---
+++ BiancaCarvalho.java
@@ -0,0 +1,13 @@
+public class BiancaCarvalho
+{
+    String nome, sobrenome;
+    int semestre;
+    
+    void nomeCompleto () {
+        
+    }
+    
+    void getSemestre() {
+        
+    }
+}
