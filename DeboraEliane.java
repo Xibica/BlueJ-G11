2efc7a2cdb0c90e102c5d8ec5928e388f60898e8
DeboraEliane <deboraeliane@discente.ufg.br>
2021-03-11 00:03:43 -0300

    class DeboraEliane

---
+++ DeboraEliane.java
@@ -0,0 +1,14 @@
+public class DeboraEliane
+{
+    int semestre;
+    String nome, sobrenome;
+
+    public String nomeCompleto(){
+       String nomeCompleto = nome+" "+sobrenome;
+       return nomeCompleto;
+    }
+    
+    public int getSemestre(){
+        return semestre;
+    }
+}
